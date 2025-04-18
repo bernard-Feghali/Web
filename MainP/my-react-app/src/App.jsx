1bfc2bca391ba32b1e3ac07b51de4fe16ccd3297
bernard-Feghali <68926743+bernard-Feghali@users.noreply.github.com>
2025-04-18 22:22:06 +0300

    MainP

---
+++ MainP/my-react-app/src/App.jsx
@@ -0,0 +1,73 @@
+
+import './App.css'
+import Navbar from './Navbar.jsx'
+import Carousel from "./Carousel";
+import FeaturesSection from "./FeaturesSection";
+import Footer from './Footer.jsx';
+const carouselImages = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=1400&q=80",
+    title: "Create",
+    alt: "Creative Design",
+    position: "text-start",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1529333166437-7750a6dd5a70?auto=format&fit=crop&w=1400&q=80",
+    title: "Personalize",
+    alt: "Customizing Products",
+    position: "text-center",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1519125323398-675f0ddb6308?auto=format&fit=crop&w=1400&q=80",
+    title: "Express",
+    alt: "Artistic Expression",
+    position: "text-end",
+  },
+];
+const features = [
+  {
+    name: "Antonios",
+    title: "Lead Designer",
+    image: "https://randomuser.me/api/portraits/men/11.jpg",
+  },
+  {
+    name: "Maria",
+    title: "Developer",
+    image: "https://randomuser.me/api/portraits/women/65.jpg",
+  },
+  {
+    name: "George",
+    title: "Marketing",
+    image: "https://randomuser.me/api/portraits/men/41.jpg",
+  },];
+
+const links = [
+  { name: "Home", href: "#" },
+  { name: "About", href: "#" },
+  { name: "Contact", href: "#" },
+];
+
+function App() {
+  return (
+    <>
+      <Navbar
+        brandName="Our Mahal Name"
+        links={links}
+        showSearch={true}
+        showAuthButtons={true}
+      />
+        <Carousel images={carouselImages} />
+        <FeaturesSection features={features} />
+        <Footer
+      year={new Date().getFullYear()}
+      company="Our Mahal Name"
+      icons={["twitter", "instagram", "facebook"]}
+    />
+    </>
+  )
+}
+
+export default App
